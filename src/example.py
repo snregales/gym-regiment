849900b881e6d61ff2b015ee7cc1b9baa9b5b560
Sharlon Regales <sharlon.regales@sogeti.com>
2019-11-03 14:18:25 +0100

    scaffolding setup for testing

--- src/example.py
+++ src/example.py
@@ -1,7 +1,19 @@
 #! /usr/bin/env python
 
-def example():
-    return "Hello, I'm the example"
+"""
+example.py
+"""
+
+def name(first: str, last: str = None) -> str:
+    """Given first and last name inject them into a sentence string
+    Arguments:
+        first {str} -- first name
+    Keyword Arguments:
+        last {str} -- last name (default: {None})
+    Returns:
+        str -- a new compound string with first and last name injected
+    """
+    return f"Hello, I'm {f'{first} {last}' if last else first}"
 
 if __name__ == '__main__':
-    print(example())
+    print(name("Sharlon", "Regales"))
--- src/tests/test.py
+++
@@ -1,4 +0,0 @@
-from src.example import example
-
-def test_example():
-    assert example() == "Hello, I'm the example"
