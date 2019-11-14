c308b12e9900b2f9ef7c22c80f3a7953b632a267
Sharlon Regales <sharlon.regales@sogeti.com>
2019-11-14 22:36:54 +0100

    Make gunicorn access flask app through autoapp instead

--- src/graphql/views.py
+++ src/graphql/views.py
@@ -5,7 +5,7 @@ Since this is going to be a graphQL application,
 The only route will be /graphql
 """
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 
 BP = Blueprint("graphql", __name__, url_prefix="/graphql", static_folder="../static")
 
@@ -17,4 +17,6 @@ def index():
 
     :return :type str: responds string
     """
+
+    current_app.logger.info("Gym Regiment's graphql page")
     return render_template("graphql/index.html")
