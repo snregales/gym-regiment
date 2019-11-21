a6d01def1339ccf015d7a252ba8b4491ab6993b7
Sharlon Regales <sharlon.regales@sogeti.com>
2019-11-21 12:48:59 +0100

    implemeent graphql in a different structure

--- src/graphql/query.py
+++ src/graphql/query.py
@@ -1,18 +1,18 @@
-from graphene import Schema, ObjectType, relay
+from graphene import Schema, ObjectType, List
 from graphene_sqlalchemy import SQLAlchemyConnectionField
 
 from src.user.schema import User, Role
 
-
 class Query(ObjectType):
     """register all schema queries are register here"""
-    node = relay.Node.Field()
-    all_users = SQLAlchemyConnectionField(User)
-    all_roles = SQLAlchemyConnectionField(Role)
+    users = List(User)
+    roles = List(Role)
+
+    def resolve_users(self, info):
+        return User.get_query(info).all()
+
+    def resolve_roles(self, info):
+        return Role.get_query(info).all()
 
 
-SCHEMA = Schema(
-    query=Query,
-    types=[
-        User,
-        Role,])
+SCHEMA = Schema(query=Query)
