10c0d4701edf5045c903a8bf91b5183ce879f2f1
Sharlon Regales <sharlonregales+dev@gmail.com>
2020-01-25 17:16:40 +0100

    Add account metrics to the system

--- src/account/__init__.py
+++ src/account/__init__.py
@@ -1,1 +1,4 @@
 """Account Module."""
+
+M_TO_FT: float = 3.2808398950131
+KG_TO_LB: float = 2.2046226218488
--- src/account/models.py
+++ src/account/models.py
@@ -1,8 +1,46 @@
 """Account's models module."""
 
+from datetime import date
+
 from src.config.database import Model, SurrogatePK, column, db
 from src.config.database import null_column as nullable
 from src.config.database import reference_col, relationship
+
+from . import KG_TO_LB, M_TO_FT
+
+
+class BioMetric(SurrogatePK, Model):
+    """
+    Accounts metrics.
+
+    This holds all the biometrics information of User
+    """
+
+    __tablename__ = "biometrics"
+
+    height = column(db.Float())  # height in Meters
+    weight = column(db.Float())  # weight in KiloGrams
+
+    @property
+    def bmi(self) -> float:
+        """Users body mass index."""
+        return self.weight / pow(self.height, 2)
+
+    @property
+    def age(self) -> int:
+        """Users age."""
+        today, dob = date.today(), self.account.dob
+        return today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+
+    @property
+    def height_in_feet(self) -> float:
+        """Impirical form of users height (Feet)."""
+        return self.height * M_TO_FT
+
+    @property
+    def weight_in_pounds(self) -> float:
+        """Impirical form of users weight (pounds)."""
+        return self.weight * KG_TO_LB
 
 
 class Account(SurrogatePK, Model):
@@ -13,17 +51,13 @@ class Account(SurrogatePK, Model):
     user_id = reference_col(tablename="users", nullable=True)
     user = relationship("User", backref="account")
 
+    biometric_id = reference_col(tablename="biometrics", nullable=True)
+    biometric = relationship("BioMetric", backref="account", uselist=False)
+
     first_name = column(db.String(30))
     last_name = column(db.String(30))
+    dob = column(db.Date)
     phone_number = nullable(db.Integer())
-
-    def __init__(self, name: str, **kwargs) -> None:
-        """
-        Create instance.
-
-        :param name :type str: name of the user
-        """
-        db.Model.__init__(self, name=name, **kwargs)
 
     @property
     def fullname(self) -> str:
